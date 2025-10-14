b983f6072cd5b431d09deab7ff9e19b2330ec5ed
Mat Gilbert <matgilbert@gmail.com>
2025-10-14 19:39:38 -0400

    wip on client / server integration

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-import { Duet } from './duet';
+import { Duet } from './duet.js';
 
 const submitButton = document.getElementById('submit');
 submitButton.on('click', async (e) => {
 
+    e.preventDefault();
     console.log('button pressed');
     const machinePosition = await Duet.getPosition();
     const outputDiv = document.getElementById('outputDiv')
